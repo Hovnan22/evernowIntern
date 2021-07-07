67815b8fd7ad20cb5f3311fcb53e0bc868e50987
Davit Harut <davit@cincopa.com>
2021-07-07 19:37:50 +0400

    initalization

---
+++ src/Navigation/index.js
@@ -0,0 +1,21 @@
+import  React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import  Home  from '../screens/Home'
+
+const Stack = createStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    View,
+    Text,
+} from "react-native"
+
+const Home = () => {
+    return (
+        <View>
+            <Text style={ {color: 'red'}}>Home</Text>
+        </View>
+    );
+}
+
+export default Home;
+
